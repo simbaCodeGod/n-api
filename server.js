f3a0e1b87e4aa2ced4eee825351f546c17afbe94
Simba <isarawadano1@gmail.com>
2023-03-24 22:07:28 +0200

    res json func

--- server.js
+++ server.js
@@ -7,5 +7,10 @@ const respondText = (req, res) => {
     res.end('hi')
 }
 
+const respondJson = (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({text: 'hi', nummbers: [1, 2, 3] }))
+}
+
 server.listen(port);
 console.log(`Server listening on port ${port}`)
