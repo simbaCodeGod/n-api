106120f3d16c08f689b5c4d5ef64e6f10feaac27
Simba <isarawadano1@gmail.com>
2023-03-24 22:05:00 +0200

    res text func

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const http = require('http')
 
 const port = process.env.PORT|| 1337
 
+const respondText = (req, res) => {
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('hi')
+}
 
 server.listen(port);
 console.log(`Server listening on port ${port}`)
