cc932c46047e43947a9abd749d65e1aa530a1090
Simba <isarawadano1@gmail.com>
2023-03-24 21:57:02 +0200

    fix req, res

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const port = process.env.PORT|| 1337
 
-const server = http.createServer(function (res, req) {
+const server = http.createServer(function (req, res) {
     res.end('hi')
 })
 
