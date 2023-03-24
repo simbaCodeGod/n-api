01f507446783e710250a6d6f312fa6bd04372941
Simba <isarawadano1@gmail.com>
2023-03-24 22:10:42 +0200

    res 404 func

--- server.js
+++ server.js
@@ -12,5 +12,10 @@ const respondJson = (req, res) => {
     res.end(JSON.stringify({text: 'hi', nummbers: [1, 2, 3] }))
 }
 
+const respondNotFound = (req, res) => {
+    res.writeHead(404, {'Content-Type' : 'text/plain'})
+    res.end('Not Found')
+}
+
 server.listen(port);
 console.log(`Server listening on port ${port}`)
