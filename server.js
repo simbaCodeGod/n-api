1e6627d92a6b210a3ebf3f7809859c30dc3b1828
Simba <isarawadano1@gmail.com>
2023-03-24 22:00:26 +0200

    server json

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const http = require('http')
 const port = process.env.PORT|| 1337
 
 const server = http.createServer(function (req, res) {
-    res.end('hi')
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({text: 'hi', numbers: [1, 2, 3]}))
 })
 
 server.listen(port);
